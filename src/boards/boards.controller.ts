45cb9b55201074ae35d616d44d0e8cf4dc80cdad
Taehoon Kim <taehoonkim.dev@gmail.com>
2023-07-10 10:07:39 +0000

    refactor: update post, delete unused code

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -9,10 +9,6 @@ import { Board } from './board.entity';
 export class BoardsController {
     constructor(private boardsService: BoardsService) { }
 
-    // @Get('/')
-    // getAllBoard(): Board[] {
-    //     return this.boardsService.getAllBoards();
-    // }
 
     @Post()
     @UsePipes(ValidationPipe)
@@ -20,39 +16,24 @@ export class BoardsController {
         return this.boardsService.createBoard(CreateBoardDto);
     }
 
-    // @Post()
-    // @UsePipes(ValidationPipe)
-    // createBoard(
-    //     @Body() createBoardDto: CreateBoardDto
-    // ): Board {
-    //     return this.boardsService.createBoard(createBoardDto);
-    // }
 
     @Get('/:id')
     getBoardById(@Param('id') id:number) : Promise<Board> {
         return this.boardsService.getBoardById(id);
     }
 
-    // @Get('/:id')
-    // getBoardById(@Param('id') id: string): Board {
-    //     return this.boardsService.getBoardById(id)
-    // }
 
     @Delete('/:id')
     deleteBoard(@Param('id', ParseIntPipe) id): Promise<void> {
         return this.boardsService.deleteBoard(id);
     }
 
-    // @Delete('/:id')
-    // deleteBoard(@Param('id') id: string): void {
-    //     this.boardsService.deleteBoard(id);
-    // }
 
-    // @Patch('/:id/status')
-    // updateBoardStatus(
-    //     @Param('id') id: string,
-    //     @Body('status', BoardStatusValidationPipe) status: BoardStatus
-    // ) {
-    //     return this.boardsService.updateBoardStatus(id, status);
-    // }
+    @Patch('/:id/status')
+    updateBoardStatus(
+        @Param('id', ParseIntPipe) id: number ,
+        @Body('status', BoardStatusValidationPipe) status: BoardStatus
+    ) {
+        return this.boardsService.updateBoardStatus(id, status);
+    }
 }
